d8297d52efeb64cfe3155be9af1b78c9db40cb48
alexonkaba4 <lagkaba@gmail.com>
2022-07-20 15:25:04 +0100

    2022 juillet 20 15:24

--- app/Http/Controllers/Responsablefinances/EcolageController.php
+++ app/Http/Controllers/Responsablefinances/EcolageController.php
@@ -13,7 +13,7 @@ use App\Models\SuiviPaiement;
 class EcolageController extends Controller
 {
     public function index(){
-        return view('Responsablefinances.Finances.Ecolages.index');
+        return view('Responsablefinances/Finances/Ecolages/index');
     }
 
     public function create(){
