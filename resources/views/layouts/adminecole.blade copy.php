6af5daba17cc64a5d0451b5701e00f3163b7d494
alexonkaba4 <lagkaba@gmail.com>
2022-08-03 12:33:50 +0100

    2022 aout 3 12:33

---
+++ resources/views/layouts/adminecole.blade copy.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    @yield('head')
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS-->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('css/design.css')}}">
+    <!--FONTS-->
+    <link rel="stylesheet" href="{{asset('fonts/font-awesome-4.7.0/css/font-awesome.css')}}">
+    <!--JS-->
+    <script src="{{asset('js/jquery-3.5.1.js')}}"></script>
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+
+    <title>
+        @yield('title')
+    </title>
+</head>
+
+@include('includes.head-tl3')
+
+<style>
+    .card-header{
+        background-color: darkblue;
+        color: white;
+    }
+    .btn-default{
+        background-color: darkblue;
+        color: white;
+    }
+    .btn-default:hover{
+        background-color: rgb(13, 13, 95);
+        color: white;
+    }
+    h4{
+        font-weight: lighter;
+        letter-spacing: 1px;
+    }
+</style>
+<body>
+    <nav class="container-fluid">
+        <div>
+            <span class="float-left"><i class="fa fa-user-circle"></i> <a href="/home" style="color: white;">{{ Auth::user()->name }}</a> | {{Auth::user()->ecole->name}}</span>
+            <span class="float-right"><strong>ADMIN ECOLE</strong></span>
+        </div>
+    </nav>
+
+    <div class="">
+        @yield('content')
+    </div>
+</body>
+</html>
