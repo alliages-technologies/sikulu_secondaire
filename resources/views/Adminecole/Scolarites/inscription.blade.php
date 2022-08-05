1daa59730a4a9aa3ac2a26dd53b4d9f7ba8e5f26
alexonkaba4 <lagkaba@gmail.com>
2022-08-05 13:08:05 +0100

    2022 aout 5 13:08

--- resources/views/Adminecole/Scolarites/inscription.blade.php
+++ resources/views/Adminecole/Scolarites/inscription.blade.php
@@ -9,28 +9,25 @@ Admin Ecole | Relévés {{ $salle->classe->name }}
 <div class="container">
     <div class="card mt-4">
         <div class="card-header">
-            <h2> RELEVES DES NOTES {{ $salle->classe->name }}  </h2>
+            <h2>GESTION DES RELEVES DES NOTES | {{ $salle->classe->name }} </h2>
         </div>
         <input type="hidden" name="salle_id" value="{{$salle->id}}">
         <input type="hidden" name="trimestre_ecole_id" value="{{$trimestre_ecole->id}}">
         <div class="card-body">
-            <div class="container-fluid">
-                <table class="table table-bordered table-striped table-hover table-sm">
-                    <thead class="">
-                        <th> NOM(S) & PRENOM(S)</th>
-                        <th>  <i class="fa fa-cog"></i> </th>
-                    </thead>
-                    <tbody>
-                        @foreach($inscriptions as $inscription)
-                        <tr>
-                            <td> {{$inscription->name}} </td>
-                            <td> <a href="/adminecole/scolarite-inscription-show/{{ $inscription->id }}/{{ $salle->ecole->token }}/{{ $salle->id }}/{{ $trimestre_ecole->id}}" class="btn btn-default btn-xs"><i class="fa fa-eye"></i></a> </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-
-            </div>
+            <table class="table table-bordered table-striped table-hover table-sm">
+                <thead class="">
+                    <th> NOM(S) & PRENOM(S) </th>
+                    <th>  <i class="fa fa-cog"></i> </th>
+                </thead>
+                <tbody>
+                    @foreach($inscriptions as $inscription)
+                    <tr>
+                        <td> {{$inscription->name}} </td>
+                        <td> <a href="/adminecole/scolarite-inscription-show/{{ $inscription->id }}/{{ $salle->ecole->token }}/{{ $salle->id }}/{{ $trimestre_ecole->id}}" class="btn btn-default btn-xs"><i class="fa fa-eye"></i></a> </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
