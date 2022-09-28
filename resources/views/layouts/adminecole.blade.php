b1e6d4470e45feae617c3b85a72585c081f7ddc8
blandin-Ux <nsondecaleb@gmail.com>
2022-09-28 13:46:42 +0100

    OK

--- resources/views/layouts/adminecole.blade.php
+++ resources/views/layouts/adminecole.blade.php
@@ -88,7 +88,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #007bff">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{asset(Auth::user()->ecole->image)}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{asset(Auth::user()->ecole->image_uri)}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">DIRECTEUR</span>
     </a>
 
