6e0e75baef3f8e5b6081edf64ed81551b112126a
blandin-Ux <nsondecaleb@gmail.com>
2022-10-05 11:15:09 +0100

    OK

--- resources/views/Adminecole/dashboard.blade.php
+++ resources/views/Adminecole/dashboard.blade.php
@@ -4,8 +4,12 @@
 @section('title')
 Admin Ecole | Acceuil
 @endsection
-
+@php
+    $ecole = App\Models\Ecole::find(Auth::user()->ecole_id);
+@endphp
 @section('content')
+<img src="{{$ecole->image_uri}}" alt="" srcset="">
+
 <div class="container menu">
     <div class="row d-flex justify-content-center p-4">
         <a style="color: red" href="/deconnexion" class="col-md-3 m-2">
