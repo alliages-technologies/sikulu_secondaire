44de7e8392f3dda66801f1656194c8af5d63a5be
blandin-Ux <nsondecaleb@gmail.com>
2022-10-06 15:04:32 +0100

    OK

--- resources/views/Superadmin/dashboard.blade.php
+++ resources/views/Superadmin/dashboard.blade.php
@@ -7,13 +7,75 @@ Superadmin | Acceuil
 
 @section('content')
 
-<div class="container menu">
-    <div class="row d-flex justify-content-center p-4">
-        <a style="color: red" href="/deconnexion" class="col-md-3 m-2">
-            <i class="fa fa-power-off"></i>
-            <p>Déconnexion</p>
-        </a>
+@php
+use App\Models\Ecole;
+
+    $ecoles = Ecole::all();
+@endphp
+
+<div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3 class="counter-count chiffre"  data-chiffre="{{$ecoles->count()}}"> {{$ecoles->count()}} </h3>
+                    <p>Nombre d'Ecoles</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">Plus d'info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3 class="counter-count"> XXXX </h3>
+                    <p>XXXX</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">Plus d'info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3 class="counter-count chiffre"> XXXX </h3>
+
+                    <p> XXXX </p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">Plus d'info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3 class="counter-count"> XXXX </h3>
+                    <p> XXXX </p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">Plus d'Info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
     </div>
 </div>
+
+
 
 @endsection
