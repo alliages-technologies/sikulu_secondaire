96e2ea093ff010434bba41ea203f17fd3c37450c
blandin-Ux <nsondecaleb@gmail.com>
2022-11-11 14:07:50 +0100

    OK

--- app/Http/Controllers/Adminecole/ProgrammeecoleController.php
+++ app/Http/Controllers/Adminecole/ProgrammeecoleController.php
@@ -79,7 +79,7 @@ class ProgrammeecoleController extends Controller
         $salle = Salle::where('token',$token)->first();
         $programme_ecole = $salle->programmeecoles->where('ecole_id', Auth::user()->ecole_id)->where('active',1)->where('annee_id',1)->first();
         $pes = ProfEcole::where('ecole_id',Auth::user()->ecole_id)->get();
-        //dd($programme_ecole);
+        dd($programme_ecole);
         return view('Adminecole.Programmeecoles.show')->with(compact('salle','programme_ecole','pes'));
     }
 
